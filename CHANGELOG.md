2e74fac1dc59ec5cf8d02dcfb3372617bff6172e
Rubayet <rubayet.evan@gmail.com>
2019-10-27 21:46:32 +0600

    publish to pub.dev 0.1.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,5 +2,6 @@
 ## [0.1.0] - Bug fixed
 ## [0.1.1] - Ability to add pre selected items
 ## [0.1.2] - Documentation Added
+## [0.1.3] - Minor Changes
 
 
--- lib/multiselectchipgroup.dart
+++ lib/multiselectchipgroup.dart
@@ -21,22 +21,23 @@ class MultiSelectChipGroup extends StatefulWidget {
   final double verticalChipSpacing;
   final List<String> preSelectedItems;
 
-  MultiSelectChipGroup({@required this.items,
-    @required this.selectedColor,
-    @required this.disabledColor,
-    this.onSelectionChanged,
-    this.labelSelectedColor,
-    this.labelDisabledColor,
-    this.leftCommonIcon,
-    this.padding,
-    this.labelFontSize,
-    this.leftIconSize,
-    this.iconDisabledColor,
-    this.iconSelectedColor,
-    this.leftIcons,
-    this.horizontalChipSpacing,
-    this.verticalChipSpacing,
-    this.preSelectedItems});
+  MultiSelectChipGroup(
+      {@required this.items,
+      @required this.selectedColor,
+      @required this.disabledColor,
+      this.onSelectionChanged,
+      this.labelSelectedColor,
+      this.labelDisabledColor,
+      this.leftCommonIcon,
+      this.padding,
+      this.labelFontSize,
+      this.leftIconSize,
+      this.iconDisabledColor,
+      this.iconSelectedColor,
+      this.leftIcons,
+      this.horizontalChipSpacing,
+      this.verticalChipSpacing,
+      this.preSelectedItems});
 
   @override
   _MultiSelectChipGroupState createState() =>
@@ -46,16 +47,13 @@ class MultiSelectChipGroup extends StatefulWidget {
 class _MultiSelectChipGroupState extends State<MultiSelectChipGroup> {
   List<String> selectedChoices = List<String>();
 
-
   _MultiSelectChipGroupState(List<String> preSelectedItems,
       Function(List<String>) onSelectionChanged) {
     if (preSelectedItems != null) {
       selectedChoices = preSelectedItems;
-      if (onSelectionChanged != null)
-        onSelectionChanged(selectedChoices);
+      if (onSelectionChanged != null) onSelectionChanged(selectedChoices);
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -70,7 +68,7 @@ class _MultiSelectChipGroupState extends State<MultiSelectChipGroup> {
           ? 0
           : widget.horizontalChipSpacing,
       runSpacing:
-      widget.verticalChipSpacing == null ? 0 : widget.verticalChipSpacing,
+          widget.verticalChipSpacing == null ? 0 : widget.verticalChipSpacing,
       children: choiceChips,
     );
   }
@@ -81,11 +79,11 @@ class _MultiSelectChipGroupState extends State<MultiSelectChipGroup> {
           fontSize: widget.labelFontSize == null ? 14 : widget.labelFontSize,
           color: selectedChoices.contains(item)
               ? widget.labelSelectedColor == null
-              ? widget.disabledColor
-              : widget.labelSelectedColor
+                  ? widget.disabledColor
+                  : widget.labelSelectedColor
               : widget.labelDisabledColor == null
-              ? widget.selectedColor
-              : widget.labelDisabledColor),
+                  ? widget.selectedColor
+                  : widget.labelDisabledColor),
       selectedColor: widget.selectedColor,
       disabledColor: widget.disabledColor,
       backgroundColor: widget.disabledColor,
@@ -94,16 +92,16 @@ class _MultiSelectChipGroupState extends State<MultiSelectChipGroup> {
       avatar: widget.leftCommonIcon == null && leftIcon == null
           ? null
           : Icon(
-        leftIcon == null ? widget.leftCommonIcon : leftIcon,
-        color: selectedChoices.contains(item)
-            ? widget.iconSelectedColor == null
-            ? widget.disabledColor
-            : widget.iconSelectedColor
-            : widget.iconDisabledColor == null
-            ? widget.selectedColor
-            : widget.iconDisabledColor,
-        size: widget.leftIconSize == null ? 16 : widget.leftIconSize,
-      ),
+              leftIcon == null ? widget.leftCommonIcon : leftIcon,
+              color: selectedChoices.contains(item)
+                  ? widget.iconSelectedColor == null
+                      ? widget.disabledColor
+                      : widget.iconSelectedColor
+                  : widget.iconDisabledColor == null
+                      ? widget.selectedColor
+                      : widget.iconDisabledColor,
+              size: widget.leftIconSize == null ? 16 : widget.leftIconSize,
+            ),
       label: Text(item),
       selected: selectedChoices.contains(item) ? true : false,
       onSelected: (selected) {
